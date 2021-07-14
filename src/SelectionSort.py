bafe7ea4a46648a81bd7fdcd3f848218ef594a26
Tanay Kapoor <itstkaay@gmail.com>
2021-07-14 18:37:05 +0530

    Update SelectionSort.py

--- src/SelectionSort.py
+++ src/SelectionSort.py
@@ -1,20 +1,21 @@
-def smallest_number(arr):
-    smallest = arr[0]
-    smallest_i = 0
-    for i in range(1, len(len)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_i = i
-    return smallest_i
+def find_smallest(arr):
+    smallest_num = arr[0]
+    smallest_num_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] < smallest_num:
+            smallest_num = arr[i]
+            smallest_num_index = i
+    return smallest_num_index
 
 
 def selection_sort(arr):
     new_arr = []
     for i in range(len(arr)):
-        smallest = smallest_number(arr)
+        smallest = find_smallest(arr)
         new_arr.append(arr.pop(smallest))
     return new_arr
 
 
-test = [98, 45, 74, 10, 36, 58, 51, 20]
-print(selection_sort(test))
+test_Arr = [65, 45, 12, 65, 2, 3, 78, 12, 46, 96, 13]
+print(selection_sort(test_Arr))
